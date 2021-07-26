e6c78ba1b0cebf3481929b45d5e6668799234b0a
Kritsada6110450014 <kritsada.lakhasont@Gmail.com>
2021-07-26 20:15:37 +0700

    Create remindMe.vue

---
+++ src/views/remindMe.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <memo-create-form></memo-create-form>
+    <memo-table></memo-table>
+  </div>
+</template>
+
+<script>
+import MemoCreateForm from '../components/MemoCreateForm.vue'
+import MemoTable from '../components/MemoTable.vue'
+
+export default {
+  components: {
+    MemoTable,
+    MemoCreateForm
+    
+  }
+}
+</script>
+
+<sty
+    MemoCreateFormle>
+
+</style>
